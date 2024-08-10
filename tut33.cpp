d74cc52ff5ee01b719754502f96561d2dc2eb866
harishabc <mrhbc2004@gmail.com>
2024-08-11 00:23:50 +0530

    added tut33.cpp

---
+++ tut33.cpp
@@ -0,0 +1,21 @@
+/* Default constructor is defining a empty constructor in side the class method*/
+#include<iostream>
+using namespace std;
+
+class BankDeposit{
+    int principal;
+    int years;
+    float interestRate;
+    float returnValue;
+    public: 
+        BankDeposit(){}
+        BankDeposit(int p,int y, int r){}
+        BankDeposit(int p,int y, float r){}
+
+
+
+};
+int main(){
+
+return 0;
+}
